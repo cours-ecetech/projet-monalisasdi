8308eb8384e422c267b6506cdd797f0da3ae071d
monalisasdi <saadimounalisa@gmail.com>
2023-03-15 16:58:18 +0000

    commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "microservice-project-2023-tfstate"
     ##==> Replace <prenom> by your firstname and <nom> by your lastname
-    key                         = "<prenom><nom>.tfstate"
+    key                         = "monalisasaadi.tfstate"
     region                      = "fr-par"
     endpoint                    = "https://s3.fr-par.scw.cloud"
     skip_credentials_validation = true
--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,18 @@
 ##==> The name of your cluster should be: k8s_cluster_<prenom><nom>
 ##==> The name of your cluster should be: k8s_pool_<prenom><nom>
 ##==> The node_type of your pool should be DEV1-M and its size should be 1
+
+
+resource "scaleway_k8s_cluster" "k8s_cluster" {
+  name    = "k8s_cluster_monalisasaadi"
+  version = "1.24.3"
+  cni     = "cilium"
+  delete_additional_resources = false
+}
+
+resource "scaleway_k8s_pool" "k8s_pool" {
+  cluster_id = scaleway_k8s_cluster.k8s_cluster.id
+  name       = "k8s_pool_monalisasaadi"
+  node_type  = "DEV1-M"
+  size       = 1
+}
